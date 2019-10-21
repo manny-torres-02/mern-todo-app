7432b2e4b9233780a60caddafd3c05c95244b9a1
Manny <manueljosetorres02@gmail.com>
2019-10-20 23:33:28 -0500

    update Constructor
    create Constructor and add in methods.

--- src/Components/create-todo.component.js
+++ src/Components/create-todo.component.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react';
 
 export default class EditTodo extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            //setting initial state of the component by assigned an object  to this.state 
+            //below are the state properties. 
+            todo_description:'',
+            todo_responsible:'',
+            todo_priority:'',
+            todo_completed:false
+        }
+
+        //methods to handle the that can be used to update the state properties. 
+        onChangeTodoDescription (e) {
+            this.setState({
+                todo_description:e.target.value
+            });
+        };
+
+        onChangeToDoResponsible(e) {
+            this.setState({
+                todo_priority:e.target.value
+            });
+        }
+
+        onChangeTodoPriority (e) {
+            this.setState({
+                todo_priority: e.target.value
+            });
+        }
+
     render () {
         return (
             <div>
